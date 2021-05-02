68e7049057bca562e841092bc74716c7916bfab7
Julie <julietaubry@gmail.com>
2021-05-02 14:39:03 +0200

    ******

--- src/js/places.js
+++ src/js/places.js
@@ -17,11 +17,12 @@ export class Places {
       let listItem = $("<div></div>").addClass("listItem");
       let itemName = $("<h3>" + this.placeName + "</h3>");
       let starsNumber = this.ratings[0].stars;
-      let star = $("<span></span>").addClass("star");
       let starContainer = $("<div></div>").addClass("star-container");
       let starList ="";
-      for (let i=0; i <= starsNumber; i++){
+      for (let i=0; i < starsNumber; i++){
         console.log(i);
+        let star = $("<span></span>").addClass("star");
+        starContainer.append(star)
       }
       starContainer.append(starList);
       console.log(starsNumber);
